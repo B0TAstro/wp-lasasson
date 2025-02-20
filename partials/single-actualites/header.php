fdc5b948c5470c1536bb4f1a0688c0c24497d5a4
B0TAstro <148534949+B0TAstro@users.noreply.github.com>
2025-02-20 14:28:36 +0100

    Commit the old theme

---
+++ partials/single-actualites/header.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays the post header
+ *
+ * @package Responsive WordPress theme
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+
+<h1 class="entry-title post-title responsive">NOTRE ACTUALITÉ</h1>
