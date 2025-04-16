5c96caeb5ad4f6ab8b62e895279c3208037de795
Tom Boullay <148534949+B0TAstro@users.noreply.github.com>
2025-04-16 11:57:51 +0200

    responsive de merde

--- header.php
+++ header.php
@@ -11,10 +11,26 @@
 
 <body>
     <header>
-        <?php $logo = get_field('logo_header', 'option'); ?>
-        <a class="logo_header" href="/">
-            <img loading="lazy" src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>">
-        </a>
+        <div class="wrapper-header">
+            <?php $logo = get_field('logo_header', 'option'); ?>
+            <?php if ($logo): ?>
+                <a class="logo_header" href="/">
+                    <img loading="lazy" src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>">
+                </a>
+            <?php endif; ?>
+
+            <nav>
+                <?php wp_nav_menu(array('menu' => 'menu-principal')); ?>
+            </nav>
+
+            <?php $btn = get_field('bouton_recrutement_lien', 'option'); ?>
+            <?php if ($btn): ?>
+                <a class="btn-recrutement" href="<?php echo esc_url($btn['url']); ?>" target="<?php echo esc_attr($btn['target']); ?>">
+                    <?php echo esc_html($btn['title']); ?>
+                </a>
+            <?php endif; ?>
+            </a>
+        </div>
 
         <input type="checkbox" id="menu-toggle" class="menu-toggle" />
         <label for="menu-toggle" class="burger" aria-label="Menu mobile">
@@ -23,12 +39,17 @@
             <span></span>
         </label>
 
-        <nav class="nav-wrapper">
-            <?php wp_nav_menu(array('menu' => 'menu-principal')); ?>
+        <div class="nav-wrapper">
+            <nav>
+                <?php wp_nav_menu(array('menu' => 'menu-principal')); ?>
+            </nav>
 
             <?php $btn = get_field('bouton_recrutement_lien', 'option'); ?>
-            <a class="btn-recrutement" href="<?php echo esc_url($btn['url']); ?>" target="<?php echo esc_attr($btn['target']); ?>">
-                <?php echo esc_html($btn['title']); ?>
+            <?php if ($btn): ?>
+                <a class="btn-recrutement" href="<?php echo esc_url($btn['url']); ?>" target="<?php echo esc_attr($btn['target']); ?>">
+                    <?php echo esc_html($btn['title']); ?>
+                </a>
+            <?php endif; ?>
             </a>
-        </nav>
+        </div>
     </header>
