29c3d34e3886d81c3bafbfd1ce0a4b8f22a8a4f8
Tom Boullay <148534949+B0TAstro@users.noreply.github.com>
2025-04-17 10:53:50 +0200

    add button "SOUTENIR"

--- template/template-home.php
+++ template/template-home.php
@@ -8,6 +8,15 @@ get_header();
 ?>
 
 <main>
+    <?php $btn = get_field('bouton_soutenir_lien', 'option'); ?>
+    <a class="btn-soutenir" href="<?php echo esc_url($btn['url']); ?>" target="<?php echo esc_attr($btn['target']); ?>">
+        <span class="icon">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/heart-empty.svg" alt="Soutenir" class="heart-empty">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/heart-full.svg" alt="Soutenir" class="heart-full">
+        </span>
+        <p class="label"><?php echo esc_html($btn['title']); ?></p>
+    </a>
+
     <?php
     $section1 = get_field('section1');
     if ($section1) :
