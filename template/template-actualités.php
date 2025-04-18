bef1c5bb738e053bea1d0913ef4a128053884e13
Tom Boullay <148534949+B0TAstro@users.noreply.github.com>
2025-04-18 11:25:36 +0200

    set up actualité

---
+++ template/template-actualités.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Template Name: Actualités
+ */
+
+get_header();
+?>
+
+<main>
+    <?php $btn = get_field('bouton_soutenir_lien', 'option'); ?>
+    <a class="btn-soutenir" href="<?php echo esc_url($btn['url']); ?>" target="<?php echo esc_attr($btn['target']); ?>">
+        <span class="icon">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/heart-empty.svg" alt="Soutenir" class="heart-empty">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/heart-full.svg" alt="Soutenir" class="heart-full">
+        </span>
+        <p class="label"><?php echo esc_html($btn['title']); ?></p>
+    </a>
+
+    <section>
+        Actualités
+    </section>
+</main>
+
+<?php
+get_footer();
+?>
