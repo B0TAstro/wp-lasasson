6755f2948e7f71846c647139858edaf8713f689e
eskoov <benjaminboget7310@gmail.com>
2025-04-24 17:01:13 +0200

    updated php and css

--- template/template-infos.php
+++ template/template-infos.php
@@ -20,35 +20,25 @@ get_header();
        
             $section_1 = get_field('section_1');
 
-     
             if ($section_1) :
                 $titre_numero_urgence = $section_1['titre_section_numero_durgence'] ?? null;
                 $contenu_numero_urgence = $section_1['texte_section_numero_durgence'] ?? null;
                 $bouton_numero_urgence = $section_1['bouton_section_numero_durgence'] ?? null;
             ?>
 
-            <?php if ($titre_numero_urgence || $contenu_numero_urgence || $bouton_numero_urgence): ?>
                 <section class="section-numero-urgence">
                     <div class="container-infos-pratiques">
-                        <div class="numero-durgence">
-                            <?php if ($titre_numero_urgence): ?>
+                        <div class="numero-durgence">                        
                                 <h2><?php echo esc_html($titre_numero_urgence); ?></h2>
-                            <?php endif; ?>
                         </div>
-                        <?php if ($contenu_numero_urgence): ?>
                             <div class="Paragraphe-numero-durgence wysiwyg">
                                 <?php echo $contenu_numero_urgence; ?>
                             </div>
-                        <?php endif; ?>
-
-                        <?php if ($bouton_numero_urgence): ?>
                                 <a href="<?php echo esc_url($bouton_numero_urgence['url']); ?>" target="_blank<?php echo esc_attr($bouton_numero_urgence['target'] ?? '_self'); ?>" class=" btn-secondary btn-infos">
                                     <?php echo esc_html($bouton_numero_urgence['title']); ?>
                                 </a>
-                        <?php endif; ?>
                     </div>
                 </section>
-            <?php endif; ?>
         <?php endif; ?>
 
         <?php
@@ -63,29 +53,20 @@ get_header();
                 $texte_bouton = $section_2['texte_bouton_pdf'] ?? null;
             ?>
 
-            <?php if ($titre_rapport || $contenu_rapport || $fichier_pdf): ?>
             <section class="section-rapport-dactivite">
                 <div class="container-infos-pratiques">
-                    <div class="titre-rapport-dactivite">
-                        <?php if ($titre_rapport): ?>
-                            <h2><?php echo esc_html($titre_rapport); ?></h2>
-                        <?php endif; ?>
+                    <div class="titre-rapport-dactivite">            
+                            <h2><?php echo esc_html($titre_rapport); ?></h2>           
                     </div>
 
-                    <?php if ($contenu_rapport): ?>
                         <div class="Paragraphe-rapport-dactivite wysiwyg">
                             <?php echo $contenu_rapport; ?>
-                        </div>
-                    <?php endif; ?>
-
-                    <?php if ($fichier_pdf): ?>
+                        </div>              
                             <a class="btn-secondary btn-infos" href="<?php echo esc_url($fichier_pdf['url']); ?>" target="_blank" download>
                                 <?php echo esc_html($texte_bouton); ?>
-                            </a>
-                    <?php endif; ?>
+                            </a>                
                 </div>
-            </section>
-            <?php endif; ?>
+            </section>        
         <?php endif; ?>
 
         <?php
@@ -101,75 +82,57 @@ get_header();
                 $repeteur_nos_partenaire = $section_3['repeteur_nos_partenaires'] ?? null;
                 ?>
 
-            <?php if ($titre_nosfinanceurs || $contenu_nosfinanceurs || $repeteur_nosfinanceurs || $titre_nos_partenaire || $contenu_nos_partenaire || $repeteur_nos_partenaire): ?>
+           
                 <section class="section-nos-financeurs-et-partenaires">
                     <div class="container-nos-financeurs-et-partenaires">
 
-                        <?php if ($titre_nosfinanceurs || $contenu_nosfinanceurs || $repeteur_nosfinanceurs): ?>
+                      
                         <div class="bloc-nosfinanceurs">
-                            <?php if ($titre_nosfinanceurs): ?>
+                           
                                 <h2><?php echo esc_html($titre_nosfinanceurs); ?></h2>
-                            <?php endif; ?>
-
-                            <?php if ($contenu_nosfinanceurs): ?>
+                                           
                                 <div class="Paragraphe-nos-financeurs wysiwyg">
                                     <?php echo $contenu_nosfinanceurs; ?>
                                 </div>
-                            <?php endif; ?>
-
-                            <?php if ($repeteur_nosfinanceurs): ?>
+                        
                                 <div class="nosfinanceurs_galerie">
                                     <?php foreach ($repeteur_nosfinanceurs as $nosfinanceurs): ?>
                                         <?php
                                             $image_nos_financeurs = $nosfinanceurs['image_du_repeteur'] ?? null;
                                             $lien_nos_financeurs = $nosfinanceurs['lien_nos_financeurs'] ?? null;
                                         ?>
-                                        <?php if ($image_nos_financeurs): ?>
+                                      
                                             <a href="<?php echo esc_url($lien_nos_financeurs); ?>" target="_blank" class="partenaire__item">
                                                 <img src="<?php echo esc_url($image_nos_financeurs['url']); ?>" alt="<?php echo esc_attr($image_nos_financeurs['alt'] ?? ''); ?>">
-                                            </a>
-                                        <?php endif; ?>
+                                            </a> 
                                     <?php endforeach; ?>
                                 </div>
-                            <?php endif; ?>
                         </div>
-                        <?php endif; ?>
-
-                        <?php if ($titre_nos_partenaire || $contenu_nos_partenaire || $repeteur_nos_partenaire): ?>
+                
                             <div class="bloc-nos-partenaires">
-                                <?php if ($titre_nos_partenaire): ?>
-                                    <h2><?php echo esc_html($titre_nos_partenaire); ?></h2>
-                                <?php endif; ?>
 
-                                <?php if ($contenu_nos_partenaire): ?>
+                                <h2><?php echo esc_html($titre_nos_partenaire); ?></h2>
+
                                     <div class="Paragraphe-nos-partenaire wysiwyg">
                                         <?php echo $contenu_nos_partenaire; ?>
                                     </div>
-                                <?php endif; ?>
-
-                                <?php if ($repeteur_nos_partenaire): ?>
+                                                   
                                     <div class="nos-partenaire_galerie">
                                         <?php foreach ($repeteur_nos_partenaire as $nos_partenaires): ?>
                                             <?php
                                                 $image_nos_partenaires = $nos_partenaires['image_du_repeteur_nos_partenaires'] ?? null;
                                                 $lien_nos_partenaires = $nos_partenaires['lien_nos_partenaires'] ?? null;
                                             ?>
-                                            <?php if ($image_nos_partenaires): ?>
                                                 <a href="<?php echo esc_url($lien_nos_partenaires); ?>" target="_blank" class="partenaire_item">
                                                     <img src="<?php echo esc_url($image_nos_partenaires['url']); ?>" alt="<?php echo esc_attr($image_nos_partenaires['alt'] ?? ''); ?>">
                                                 </a>
-                                            <?php endif; ?>
                                         <?php endforeach; ?>
                                     </div>
-                                <?php endif; ?>
                             </div>
-                        <?php endif; ?>
                     </div>
                 </section>
-            <?php endif; ?>
         <?php endif; ?>
     </main>
-
 <?php
 get_footer();
 ?>
